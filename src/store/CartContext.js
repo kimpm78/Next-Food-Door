ee8864fc844c96a617a19e38ccfdea650d64c1f8
kimpm78 <kimsoosang12@gmail.com>
2024-04-05 13:00:57 +0900

    header-ui-fix

--- src/store/CartContext.js
+++ src/store/CartContext.js
@@ -1,13 +1,31 @@
-import { createContext } from "react";
-import PropTypes from "prop-types";
-// TODO: Think Context API Clean Code
-const CartContext = createContext();
-const Context = (props) => {
-  return <CartContext.Provider>{props.children}</CartContext.Provider>;
-};
+// import { createContext } from "react";
+// import PropTypes from "prop-types";
+// // TODO: Think Context API Clean Code
+// export const CartContext = createContext();
+// // export const Context = (props) => {
+// //   const reducer = (state, action) => {
+// // switch (action.type) {
+// //   case "ADD":
+// //     const tempState = state.filter((item) => action.payload.id === item.id);
+// //     if (tempState.length > 0) {
+// //       return state;
+// //     } else {
+// //       return [...state, action.payload];
+// //     }
+// //   case "INCREASE":
+// //     const tempState1 = state.map((item) => {
+// //       if (item.id === action.payload.id) {
+// //         return { ...item, quantity: item.quantity + 1 };
+// //       } else {
+// //         return item;
+// //       }
+// //     });
+// // }
+// // };
 
-Context.propTypes = {
-  children: PropTypes.node,
-};
+// //   return <CartContext.Provider>{props.children}</CartContext.Provider>;
+// // };
 
-export default Context;
+// Context.propTypes = {
+//   children: PropTypes.node,
+// };
