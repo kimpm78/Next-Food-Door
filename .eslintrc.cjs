300a09736253760eb193ca05478fc474f51a34e9
kimpm78 <kimsoosang12@gmail.com>
2026-05-07 19:11:20 +0900

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  ignorePatterns: ["build/", "public/*.html"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: { version: "detect" },
+  },
+};
